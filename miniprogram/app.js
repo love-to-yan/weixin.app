05d3fe1fe93e0c63a4a080839c6e467d85b548bd
love-to-yan <1757478118@qq.com>
2018-12-17 15:49:11 +0800

    npm i wx-server-sdk
    npm i wx-server-sdk

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,5 +1,7 @@
- onLaunch: function () {
-    
+App({
+ 
+  onLaunch: function () {
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -11,3 +13,4 @@
     this.globalData = {}
   }
 
+}) 
